cb0c653f5d91b54ada3153aaf5af68320899a5b8
charles806 <c08445333@gmail.com>
2026-09-02 17:52:20 +0100

    React-ecosystem packages in one chunk

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,5 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-          mui: ["@mui/material", "@mui/icons-material", "@emotion/react", "@emotion/styled"],
-          ui: ["sonner", "lucide-react", "react-icons"],
-        },
-      },
-    },
   },
 });
